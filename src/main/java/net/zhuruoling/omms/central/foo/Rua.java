2e3e7545a90681b74e710521ec752f32b2b2c492
ZhuRuoLing <jkl-963@live.com>
2023-05-19 23:59:33 +0800

    merge

--- src/main/java/net/zhuruoling/omms/central/foo/Rua.java
+++ src/main/java/net/zhuruoling/omms/central/foo/Rua.java
@@ -1,26 +1,44 @@
 package net.zhuruoling.omms.central.foo;
 
 
-import cn.hutool.core.exceptions.ExceptionUtil;
-import net.zhuruoling.omms.central.network.session.response.Result;
-import net.zhuruoling.omms.central.util.UtilKt;
+import cn.hutool.core.util.ClassLoaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
 
 public class Rua {
     private static final Logger logger = LoggerFactory.getLogger("Rua");
 
     public static void main(String[] args) throws Throwable {
-        //SysOutOverSLF4J.sendSystemOutAndErrToSLF4J();
-        Arrays.stream(Result.values()).map(Enum::toString).sorted().forEach(it -> {
-            System.out.printf("%s, \n", it);
+        var clazzUnixMountEntry = ClassLoaderUtil.getClassLoader().loadClass("sun.nio.fs.UnixMountEntry");
+        var clazzUnixFileStore = ClassLoaderUtil.getClassLoader().loadClass("sun.nio.fs.UnixFileStore");
+        FileSystems.getDefault().getFileStores().forEach(fileStore -> {
+            var store = clazzUnixFileStore.isInstance(fileStore);
+            try {
+                Field entryField = null;
+                for (Field field : clazzUnixFileStore.getDeclaredFields()) {
+                    if (field.getType() == clazzUnixMountEntry){
+                        System.out.println(field.getName());
+                        System.out.println(field.getType().getName());
+                        entryField = field;
+                        break;
+                    }
+                }
+                if (entryField == null){
+                    throw new NullPointerException("entryField");
+                }
+                entryField.setAccessible(true);
+                var mountEntry = entryField.get(store);
+                var dirField = clazzUnixMountEntry.getField("dir");
+                dirField.setAccessible(true);
+                var dir = (byte[]) dirField.get(mountEntry);
+                System.out.println(new String(dir, StandardCharsets.UTF_8));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         });
-
     }
 }
--- src/main/java/net/zhuruoling/omms/central/foo/Test.kt
+++ src/main/java/net/zhuruoling/omms/central/foo/Test.kt
@@ -1,16 +1,19 @@
 package net.zhuruoling.omms.central.foo
 
+import org.apache.tools.ant.taskdefs.Classloader
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J
+import java.nio.file.FileSystems
 
 val logger: Logger = LoggerFactory.getLogger("Test")
 
 
+
+
 fun main() {
-    //SysOutOverSLF4J.sendSystemOutAndErrToSLF4J()
-    val str = "replace|keep|outline|hollow|destroy"
-    str.split("|").forEach {
-        print("\"$it\",")
+    FileSystems.getDefault().fileStores.forEach{
+
+        println("$it type: ${it.type()}  name: ${it.name()}  dir: ${it}")
     }
 }
