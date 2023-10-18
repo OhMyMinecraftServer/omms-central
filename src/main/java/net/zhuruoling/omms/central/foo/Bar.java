226bbd8f50d1965218654e16a81af68e7254c669
ZhuRuoLing <jkl-963@live.com>
2023-10-18 11:20:47 +0800

    feat: system identity

--- src/main/java/net/zhuruoling/omms/central/foo/Bar.java
+++ src/main/java/net/zhuruoling/omms/central/foo/Bar.java
@@ -1,20 +1,6 @@
 package net.zhuruoling.omms.central.foo;
 
 
-import net.zhuruoling.omms.central.command.BuiltinCommand;
-import net.zhuruoling.omms.central.command.CommandManager;
-import net.zhuruoling.omms.central.command.CommandSourceStack;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.CompletableFuture;
-
 public class Bar {
     public static void main(String[] args) {
 
