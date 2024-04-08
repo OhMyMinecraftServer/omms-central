605c3191424bd1e495f476a7fad234dc437b54f5
ZhuRuoLing <jkl-963@live.com>
2024-04-08 23:13:10 +0800

    publish to NekoMaven

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,16 +22,45 @@ plugins {
 group = "icu.takeneko"
 version = properties["version"]!!
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+getComponents().withType(AdhocComponentWithVariants::class.java).forEach { c ->
+    c.withVariantsFromConfiguration(project.configurations.shadowRuntimeElements.get()) {
+        skip()
+    }
+}
+
 publishing {
     repositories {
         mavenLocal()
+        maven {
+            name = "NekoMavenRelease"
+            url = uri("https://maven.takeneko.icu/releases")
+            credentials{
+                username = project.findProperty("nekomaven.user")as String? ?: System.getenv("NEKO_USERNAME")
+                password = project.findProperty("nekomaven.password")as String? ?: System.getenv("NEKO_TOKEN")
+            }
+        }
     }
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
         }
     }
 }
+
+println(configurations["shadow"])
 
 application {
     mainClass.set("icu.takeneko.omms.central.main.Main")
@@ -97,46 +126,46 @@ tasks {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-auth:2.0.2")
-    implementation("io.ktor:ktor-server-auth-jvm:2.0.2")
-    implementation("uk.org.lidalia:sysout-over-slf4j:1.0.2")
-    implementation("org.jline:jline:3.21.0")
-    implementation("com.mojang:brigadier:1.0.18")
-    implementation("io.ktor:ktor-server-call-logging-jvm:2.0.2")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.0.2")
-    implementation("io.ktor:ktor-server-core-jvm:2.0.2")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.0.2")
-    implementation("io.ktor:ktor-serialization-gson-jvm:2.0.2")
-    implementation("io.ktor:ktor-server-netty-jvm:2.0.2")
-    implementation("com.google.code.gson:gson:2.9.0")
-    implementation("org.slf4j:slf4j-api:1.7.36")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("ch.qos.logback:logback-core:1.2.11")
-    implementation("org.jetbrains:annotations:23.0.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
-    implementation("io.netty:netty-all:4.1.77.Final")
-    implementation("com.github.oshi:oshi-core:6.1.6")
-    implementation("net.java.dev.jna:jna:5.11.0")
-    implementation("net.java.dev.jna:jna-platform:5.11.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.0.2")
-    implementation("io.ktor:ktor-server-content-negotiation:2.0.2")
-    implementation("me.xdrop:fuzzywuzzy:1.4.0")
-    implementation("io.ktor:ktor-client-core-jvm:2.1.2")
-    implementation("io.ktor:ktor-client-cio:2.1.2")
-    implementation("io.ktor:ktor-client-cio-jvm:2.1.2")
-    implementation("io.ktor:ktor-client-websockets:2.1.2")
-    implementation("io.ktor:ktor-client-auth-jvm:2.1.2")
-    implementation("io.ktor:ktor-client-auth:2.1.2")
-    implementation("commons-io:commons-io:2.11.0")
-    implementation("cn.hutool:hutool-all:5.8.11")
-    implementation("io.ktor:ktor-http:2.2.3")
-    implementation("com.github.gotson:sqlite-jdbc:3.32.3.8")
-    implementation("io.ktor:ktor-client-serialization:2.2.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.ktor:ktor-server-websockets-jvm:2.0.2")
-    implementation("net.bytebuddy:byte-buddy-agent:1.14.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.5.0")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-$target:$versionSkiko")
+    api("io.ktor:ktor-server-auth:2.0.2")
+    api("io.ktor:ktor-server-auth-jvm:2.0.2")
+    api("uk.org.lidalia:sysout-over-slf4j:1.0.2")
+    api("org.jline:jline:3.21.0")
+    api("com.mojang:brigadier:1.0.18")
+    api("io.ktor:ktor-server-call-logging-jvm:2.0.2")
+    api("io.ktor:ktor-server-content-negotiation-jvm:2.0.2")
+    api("io.ktor:ktor-server-core-jvm:2.0.2")
+    api("io.ktor:ktor-serialization-kotlinx-json-jvm:2.0.2")
+    api("io.ktor:ktor-serialization-gson-jvm:2.0.2")
+    api("io.ktor:ktor-server-netty-jvm:2.0.2")
+    api("com.google.code.gson:gson:2.9.0")
+    api("org.slf4j:slf4j-api:1.7.36")
+    api("ch.qos.logback:logback-classic:1.2.11")
+    api("ch.qos.logback:logback-core:1.2.11")
+    api("org.jetbrains:annotations:23.0.0")
+    api("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
+    api("io.netty:netty-all:4.1.77.Final")
+    api("com.github.oshi:oshi-core:6.1.6")
+    api("net.java.dev.jna:jna:5.11.0")
+    api("net.java.dev.jna:jna-platform:5.11.0")
+    api("io.ktor:ktor-serialization-kotlinx-json:2.0.2")
+    api("io.ktor:ktor-server-content-negotiation:2.0.2")
+    api("me.xdrop:fuzzywuzzy:1.4.0")
+    api("io.ktor:ktor-client-core-jvm:2.1.2")
+    api("io.ktor:ktor-client-cio:2.1.2")
+    api("io.ktor:ktor-client-cio-jvm:2.1.2")
+    api("io.ktor:ktor-client-websockets:2.1.2")
+    api("io.ktor:ktor-client-auth-jvm:2.1.2")
+    api("io.ktor:ktor-client-auth:2.1.2")
+    api("commons-io:commons-io:2.11.0")
+    api("cn.hutool:hutool-all:5.8.11")
+    api("io.ktor:ktor-http:2.2.3")
+    api("com.github.gotson:sqlite-jdbc:3.32.3.8")
+    api("io.ktor:ktor-client-serialization:2.2.3")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    api("io.ktor:ktor-server-websockets-jvm:2.0.2")
+    api("net.bytebuddy:byte-buddy-agent:1.14.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.5.0")
+    api("org.jetbrains.skiko:skiko-awt-runtime-$target:$versionSkiko")
 }
 
 task("generateProperties") {
