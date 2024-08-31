6f77b31fdb15f0feccbddd63a53e054a8db67be7
ZhuRuoLing <jkl-963@live.com>
2024-08-31 19:14:20 +0800

    chore: bump protocol version

--- src/main/java/icu/takeneko/omms/central/fundation/Constants.java
+++ src/main/java/icu/takeneko/omms/central/fundation/Constants.java
@@ -4,7 +4,7 @@ import icu.takeneko.omms.central.network.chatbridge.Target;
 import icu.takeneko.omms.central.network.session.request.LoginRequest;
 
 public class Constants {
-    public static final long PROTOCOL_VERSION = LoginRequest.VERSION_BASE + 0x11;
+    public static final long PROTOCOL_VERSION = LoginRequest.VERSION_BASE + 0x12;
     public static final String PRODUCT_NAME = "Oh My Minecraft Server Central Server";
     public static final String PRODUCT_NAME_SHORT = "OMMS Central Server";
     public static final Target TARGET_CHAT = new Target("224.114.51.4", 10086);
